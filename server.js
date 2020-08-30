71f4684ae57c99840f91db862ed9acc0d21484f8
Brad Fabian <bradleofabian@gmail.com>
2020-08-30 17:42:27 -0400

    made a custom logger for dev environment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotEnv = require("dotenv");
-
+const logger = require("./middleware/logger");
+const morgan = require("morgan");
 // Route Files
 const bootcamps = require("./routes/bootcamp");
 
@@ -8,6 +9,11 @@ const bootcamps = require("./routes/bootcamp");
 dotEnv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// Dev logging Middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
